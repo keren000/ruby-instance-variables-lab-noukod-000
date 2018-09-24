ef6019ccb92c30a9d5fa04f232aeba50763e4b62
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-24 18:45:07 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,41 @@
+class Dog
+
+  def initialize(name)
+
+    @myname = name
+
+  end
+
+
+
+  def set_name( aName )
+
+    @myname = aName
+
+  end
+
+
+
+  def get_name
+
+    return @myname
+
+  end
+
+
+
+  def talk
+
+    return 'woof!'
+
+  end
+
+end
+
+
+
+mydog = Dog.new('Fido')
+
+puts(mydog.get_name)
+
+puts(mydog.talk)
